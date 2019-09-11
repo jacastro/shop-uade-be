999273af17ff93a65e35ee7c11d61a59338f8b1d
marianogrimaux <mariano.grimaux@gmail.com>
2019-09-11 16:50:49 -0300

    Add first approach of Item class.

---
+++ src/main/java/ar/edu/uade/integracion/shop/entity/Item.java
@@ -0,0 +1,73 @@
+package ar.edu.uade.integracion.shop.entity;
+
+import javax.persistence.Id;
+import java.util.List;
+
+public class Item {
+
+    @Id
+    private String id;
+    private User publisher;
+    private float price;
+    private String name;
+    private String description;
+    private List<String> photos;
+
+
+    public Item(String id, User publisher, float price, String name, String description, List<String> photos) {
+        this.id = id;
+        this.publisher = publisher;
+        this.price = price;
+        this.name = name;
+        this.description = description;
+        this.photos = photos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(User publisher) {
+        this.publisher = publisher;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
+}
