cb87928898588fbae1413b8d0d20f419fe89ca8c
marianogrimaux <mariano.grimaux@gmail.com>
2019-09-18 19:47:20 -0300

    Add DTOs.

---
+++ src/main/java/ar/edu/uade/integracion/shop/entity/ItemDto.java
@@ -0,0 +1,65 @@
+package ar.edu.uade.integracion.shop.entity;
+
+import java.util.List;
+
+public class ItemDto {
+
+    private int id;
+    private UserDto seller;
+    private float price;
+    private String name;
+    private String description;
+    private List<String> photos;
+    // TODO : make it ENUM
+    private String warranty;
+    private Category category;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UserDto getSeller() {
+        return seller;
+    }
+
+    public void setSeller(UserDto seller) {
+        this.seller = seller;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
+}
